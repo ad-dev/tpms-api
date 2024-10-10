d4f79ca38017edd25af9caf426903a06ba6b6285
AD <ilg@xad.lt>
2024-10-10 14:50:30 +0300

    Truck repository

---
+++ src/Repository/TruckRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Truck;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Truck>
+ */
+class TruckRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Truck::class);
+    }
+
+    public function geTotalCount(): int
+    {
+        return $this->createQueryBuilder('t')->select('COUNT(1)')->getQuery()->getSingleScalarResult();
+
+    }
+}
