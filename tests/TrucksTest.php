c41ce65166490d8b00d4ef807c668917af2fd6e0
AD <ilg@xad.lt>
2024-10-10 14:59:07 +0300

    Trucks test

---
+++ tests/TrucksTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Tests;
+use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
+use App\DataFixtures\TruckFixures;
+use App\Repository\DriverRepository;
+use App\Repository\FleetRepository;
+use App\Repository\TruckRepository;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class TrucksTest extends ApiTestCase
+{
+
+    public function testTrucks(): void
+    {
+        $truckRepository = static::getContainer()->get(TruckRepository::class);
+
+        $trucks = $truckRepository->findAll();
+
+        $serializer = static::getContainer()->get(SerializerInterface::class);
+
+        $trucksExpected = $serializer->serialize($trucks, 'json');
+
+        $response = static::createClient()->request('GET', '/trucks');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertEquals($trucksExpected, $response->getContent());
+    }
+}
