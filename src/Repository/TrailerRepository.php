4108f33d013d384736c517be70b4334e721f7dba
AD <ilg@xad.lt>
2024-10-10 14:52:11 +0300

    Trailer repository

---
+++ src/Repository/TrailerRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Trailer;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Trailer>
+ */
+class TrailerRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Trailer::class);
+    }
+
+
+    public function geTotalCount(): int
+    {
+        return $this->createQueryBuilder('t')->select('COUNT(1)')->getQuery()->getSingleScalarResult();
+
+    }
+}
