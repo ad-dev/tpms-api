fcb7f61e9629fd4c71a5b1ed52e9fbcf5311264a
AD <ilg@xad.lt>
2024-10-10 14:57:45 +0300

    Drivers test

---
+++ tests/DriversTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Tests;
+use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
+use App\Repository\DriverRepository;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class DriversTest extends ApiTestCase
+{
+
+    public function testDrivers(): void
+    {
+        $driverRepository = static::getContainer()->get(DriverRepository::class);
+
+        $drivers = $driverRepository->findAll();
+
+        $serializer = static::getContainer()->get(SerializerInterface::class);
+
+        $driversExpected = $serializer->serialize($drivers, 'json');
+
+        $response = static::createClient()->request('GET', '/drivers');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertEquals($driversExpected, $response->getContent());
+    }
+}
